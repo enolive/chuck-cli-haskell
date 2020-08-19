caa14d4aaaa8eb64191c5c2742c6a77913cc244a
Christoph Welcz <christoph@welcz.de>
2020-08-19 22:26:29 +0200

    :recycle: improved api by using different joke provider

--- app/Main.hs
+++ app/Main.hs
@@ -1,74 +1,56 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Lens
 import Data.Aeson
-import Data.Aeson.Lens
-import Data.Text (Text, intercalate)
+import Data.Text
+import qualified Data.Text.IO as TIO
+import GHC.Generics (Generic)
 import Network.HTTP.Simple
 import System.Environment (getArgs)
 
-type ErrorType = Text
+newtype JokeResponse = JokeResponse {value :: Text}
+  deriving (Generic, Show, FromJSON)
 
-type AppResult = Either ErrorType Text
+newtype CategoriesResponse = CategoriesResponse [Text]
+  deriving (Generic, Show, FromJSON)
 
-data ApiResponse = ApiResponse
-  { resultType :: ResultType,
-    value :: Value
-  }
-  deriving (Show)
+host :: String
+host = "https://api.chucknorris.io/jokes"
 
-data ResultType = SuccessfulResult | ErrorResult Text deriving (Show)
-
-instance FromJSON ResultType where
-  parseJSON = withText "type" $ \case
-    "success" -> return SuccessfulResult
-    err -> return $ ErrorResult err
-
--- as type is a reserved keyword, we need to decode the JSON by hand
-instance FromJSON ApiResponse where
-  parseJSON =
-    withObject "Joke" $ \v -> ApiResponse <$> v .: "type" <*> v .: "value"
-
-fetchRandomJoke :: IO AppResult
+fetchRandomJoke :: IO Text
 fetchRandomJoke =
-  httpJSON "https://api.icndb.com/jokes/random"
-    <&> getResponseBody
-    <&> extractJoke
+  value . getResponseBody
+    <$> (parseRequest (host <> "/random") >>= httpJSON)
 
-extractJoke :: ApiResponse -> AppResult
-extractJoke ApiResponse {resultType = SuccessfulResult, value = v} = Right $ v ^. key "joke" . _String
-extractJoke ApiResponse {resultType = ErrorResult err, value = v} = extractError err v
-
-extractError :: Text -> Value -> AppResult
-extractError err v = Left $ err <> ": " <> v ^. _String
-
-fetchRandomJokeByCategory :: String -> IO AppResult
+fetchRandomJokeByCategory :: String -> IO Text
 fetchRandomJokeByCategory cat =
-  parseRequest ("https://api.icndb.com/jokes/random?limitTo=" <> cat)
-    >>= httpJSON
-    <&> getResponseBody
-    <&> extractJoke
+  value . getResponseBody
+    <$> (parseRequestThrow (host <> "/random?category=" <> cat) >>= httpJSON)
 
-fetchCategories :: IO AppResult
+fetchCategories :: IO Text
 fetchCategories =
-  httpJSON "https://api.icndb.com/categories"
-    <&> getResponseBody
-    <&> extractCategories
+  mkString . getResponseBody
+    <$> (parseRequest (host <> "/categories") >>= httpJSON)
+  where
+    mkString :: CategoriesResponse -> Text
+    mkString (CategoriesResponse cs) = intercalate ", " cs
 
-extractCategories :: ApiResponse -> AppResult
-extractCategories ApiResponse {resultType = SuccessfulResult, value = v} = Right . intercalate ", " $ v ^.. _Array . each . _String
-extractCategories ApiResponse {resultType = ErrorResult err, value = v} = extractError err v
+doAction :: [String] -> IO Text
+doAction [] = usage
+doAction ["joke"] = fetchRandomJoke
+doAction ("joke" : cat : _) = fetchRandomJokeByCategory cat
+doAction ("categories" : _) = fetchCategories
+doAction _ = fetchRandomJoke
+
+usage :: IO Text
+usage =
+  return
+    "usage\n \
+    \\tchuck joke [<category>]   # displays a random joke. lets you choose the <category> optionally\n\
+    \\tchuck categories          # displays the available categories\n"
 
 main :: IO ()
-main = do
-  args <- getArgs
-  result <- doAction args
-  print result
-
-doAction :: [String] -> IO AppResult
-doAction [] = fetchRandomJoke
-doAction ("categories" : _) = fetchCategories
-doAction (cat : _) = fetchRandomJokeByCategory cat
+main = getArgs >>= doAction >>= TIO.putStrLn
